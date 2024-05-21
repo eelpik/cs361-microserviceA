079ada3ff86453658b70cddfe9963d9a0953a10e
eelpik <122061802+eelpik@users.noreply.github.com>
2024-05-20 17:27:06 -0700

    Update extension-attacher.py

--- extension-attacher.py
+++ extension-attacher.py
@@ -10,32 +10,33 @@ excluded_dirs = [".venv", ".git", "__pycache__"]
 def attach_extension():
     data = request.get_json()
 
+    # try getting data from request
     try:
-        file_name = data["file-name"]
+        name = data["name"]
     except KeyError:
-        abort(400, {"message": "Request is missing file-name"})
+        abort(400, {"message": "Request is missing name"})
 
     try:
-        file_extension = data["file-extension"]
+        extension = data["extension"]
     except KeyError:
-        abort(400, {"message": "Request is missing file-extension"})
+        abort(400, {"message": "Request is missing extension"})
 
-    full_filename = file_name + "." + file_extension.strip(".")
+    # attach name and extension
+    filename = name + "." + extension.strip(" .")
 
     # get list of matching files
-    file_paths = []
+    filepaths = []
     for root, dirs, files in os.walk(".", topdown=True):
         dirs[:] = [dir for dir in dirs if dir not in excluded_dirs]
         for file in files:
-            if file == full_filename:
-                file_path = os.path.join(root, file)
-                file_paths.append(file_path)
-
-    # file_extension = file_extension.strip(" .")
+            if file == filename:
+                filepath = os.path.join(root, file)
+                filepath = os.path.abspath(filepath)
+                filepaths.append(filepath)
 
     return {
-        "file-name": full_filename,
-        "file-paths": file_paths
+        "filename": filename,
+        "filepaths": filepaths
     }
 
 
